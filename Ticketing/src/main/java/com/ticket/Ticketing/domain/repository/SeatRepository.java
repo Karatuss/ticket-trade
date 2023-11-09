063b2af51423d2113d25758115193cade2d635f8
tigris24 <kyrae604@naver.com>
2023-11-09 17:29:17 +0900

    Delete no use Repository

--- Ticketing/src/main/java/com/ticket/Ticketing/domain/repository/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ticket.Ticketing.domain.repository;
-
-import com.ticket.Ticketing.domain.document.SeatDocument;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatRepository extends CrudRepository<SeatDocument, String> {
-}
