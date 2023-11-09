a3d2365fda1843e8bdd6c35fa164524005c3634d
tigris24 <kyrae604@naver.com>
2023-11-10 00:51:47 +0900

    Update UserService
    create updateUser for POST seat, registerUser for POST register

--- Ticketing/src/main/java/com/ticket/Ticketing/service/UserService.java
+++ Ticketing/src/main/java/com/ticket/Ticketing/service/UserService.java
@@ -1,79 +1,80 @@
 package com.ticket.Ticketing.service;
 
-import com.ticket.Ticketing.domain.document.UserDocument;
-import com.ticket.Ticketing.domain.repository.UserRepository;
-import com.ticket.Ticketing.dto.UserDto;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.kv.ExistsResult;
+import com.ticket.Ticketing.config.UserConfig;
+import com.ticket.Ticketing.domain.repository.Gender;
+import com.ticket.Ticketing.domain.repository.Role;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.ticket.Ticketing.Ticketing.cluster;
 
 
 @Service
 @AllArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
 
-    private UserDocument createUserDocument(UserDto userDto){
-        return UserDocument.builder()
-                .id(userDto.getId())
-                .age(userDto.getAge())
-                .name(userDto.getName())
-                .email(userDto.getEmail())
-                .password(userDto.getPassword())
-                .phoneNumber(userDto.getPhoneNumber())
-                .gender(userDto.getGender())
-                .seat(userDto.getSeat())
-                .role(userDto.getRole())
-//                .faceImg(couchDto.getFaceImg())
-                .build();
-    }
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-    public void setList(UserDto userDto){
-        userRepository.save(this.createUserDocument(userDto));
-    }
-
-
-/*
-    // convert document to dto
-    private UserDto convertDocumentToDto(UserDocument userDocument) {
-        return UserDto.builder()
-                .id(userDocument.getId())
-                .age(userDocument.getAge())
-                .name(userDocument.getName())
-                .email(userDocument.getEmail())
-                .password(userDocument.getPassword())
-                .phoneNumber(userDocument.getPhoneNumber())
-                .gender(userDocument.getGender())
-                .seat(userDocument.getSeat())
-                .role(userDocument.getRole())
-                .loginAttempts(userDocument.getLoginAttempts())
-//                .faceImg(userDocument.getFaceImg())
-                .build();
-
-    }
-
-    // READ
-    public List<UserDto> getList(String id, String password) {
-        List<UserDto> userDtos = new ArrayList<>();
-        List<UserDocument> userDocuments = (List<UserDocument>) userRepository.findAll();
-        for(UserDocument userDocument : userDocuments){
-            userDtos.add(this.convertDocumentToDto(userDocument));
-        }
-        return userDtos;
-    }
-
-    public List<String> getBookedSeat(String userId){
+    public void updateUser(String loginUser, List<Integer> seatList) {
         Bucket userBucket = cluster.bucket(UserConfig.getStaticBucketName());
         Collection userCollection = userBucket.defaultCollection();
 
-        GetResult userResult = userCollection.get(userId);
-        JsonObject content = userResult.contentAsObject();
+        // update seat info from user bucket
+        JsonObject revisedInfo = userCollection.get(loginUser).contentAsObject();
+        revisedInfo.put("seat", seatList);
 
-        List<String> userSeatList = (List<String>) content.get("seat");
-        //TODO 나중에 받은 데이터 seat인지도 확인해야함
-
-        return userSeatList;
+        userCollection.replace(loginUser, revisedInfo);
     }
 
-    */
+    public void registerUser(HashMap<String, Object> registerData) {
+        Bucket userBucket = cluster.bucket(UserConfig.getStaticBucketName());
+        Collection userCollection = userBucket.defaultCollection();
+
+        String n_ID = String.valueOf(registerData.get("n_ID"));
+        String email = String.valueOf(registerData.get("email"));
+        String phoneNumber = String.valueOf(registerData.get("phoneNumber"));
+
+        ExistsResult resultID = userCollection.exists(n_ID);
+        ExistsResult resultPN = userCollection.exists(phoneNumber);
+        ExistsResult resultEmail = userCollection.exists(email);
+
+        // check inputted id, phoneNumber, email from DB
+        if (resultID.exists() | resultPN.exists() | resultEmail.exists()) {
+            throw new DuplicateKeyException("There are duplicated ID, phone number or email. ");
+        }
+
+        // put data into couchbase
+        Gender gender = Gender.valueOf(String.valueOf(registerData.get("gender")));
+        String rawPassword = String.valueOf(registerData.get("n_PW"));
+        String EncPW = passwordEncoder.encode(rawPassword);
+
+        // data for saving to couchbase
+        JsonObject jsonData = JsonObject.create()
+                .put("id", n_ID)
+                .put("password", EncPW)
+                .put("name", String.valueOf(registerData.get("username")))
+                .put("age", Integer.valueOf(String.valueOf(registerData.get("age"))))
+                .put("email", email)
+                .put("gender", String.valueOf(gender))
+                .put("phoneNumber", phoneNumber)
+                .put("seat", Collections.nCopies(2, null))
+                .put("role", String.valueOf(Role.MEMBER))
+                .put("loginAttempts", 0);
+
+        // insert data
+        userCollection.insert(n_ID, jsonData);
+    }
 
 }
