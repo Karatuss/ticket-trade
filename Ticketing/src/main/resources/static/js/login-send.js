0e929d01d571fa177ad4c60a7afd5488be18b8fb
greenapple <dla16253745@gmail.com>
2023-11-13 04:23:59 +0900

    Update: Delete and modify unused things

--- Ticketing/src/main/resources/static/js/login-send.js
+++ Ticketing/src/main/resources/static/js/login-send.js
@@ -1,6 +1,6 @@
-function login(){
-    var id = document.getElementById("user_ID").value;
-    var password = document.getElementById("user_PW").value;
+function login() {
+    let id = document.getElementById("user_ID").value;
+    let password = document.getElementById("user_PW").value;
 
     //서버에 보낼 id정보와 password 정보를 data라는 형식의 파일에 저장
     var data = {
@@ -26,10 +26,9 @@ function login(){
     fetch('http://localhost:8080/login', requestOptions)
         // 서버로부터 받은 응답을 response에 json 형식으로 파싱 (response는 서버에서 받은 전체 응답을 의미)
         .then(response => {
-            if(!(response.ok)) {
+            if (!(response.ok)) {
                 throw new Error('네트워크 오류');
-            }
-            else if(response.service_unavailable){
+            } else if (response.service_unavailable) {
                 alert('로그인 시도 횟수를 초과하였습니다.\n 다음에 다시 시도해 주세요')
             }
             return response.json();
@@ -37,21 +36,18 @@ function login(){
 
         // 서버로 부터 받은 본문의 내용을 data에 저장 후 응답 처리
         .then(data => {
-            if(data.loginSuccess){
+            if (data.loginSuccess) {
                 alert('로그인 되었습니다!');
                 //
-                if(data.identify === "MEMBER"){
-                    window.location.href = 'seat';
-                }
-                else if(data.identify === "MANAGER"){
-                    window.location.href = 'manager';
-                }
-                else{
+                if (data.identify === "MEMBER") {
+                    window.location.href = 'user-event';
+                } else if (data.identify === "MANAGER") {
+                    window.location.href = 'manager-event';
+                } else {
                     alert('identify 오류');
                 }
 
-            }
-            else{
+            } else {
                 alert('로그인 실패하였습니다. 다시 시도해주세요.');
                 document.getElementById("user_ID").value = "";
                 document.getElementById("user_PW").value = "";
@@ -61,14 +57,14 @@ function login(){
         .catch(error => console.error('Error', error));
 }
 
-document.addEventListener("keyup", function(event){
-    if(event.code === 'Enter'){
+document.addEventListener("keyup", function (event) {
+    if (event.code === 'Enter') {
         login();
     }
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    var inputElement = document.getElementById('user_ID');
+document.addEventListener('DOMContentLoaded', function () {
+    const inputElement = document.getElementById('user_ID');
     inputElement.focus(); // 자동으로 포커스 주기
 });
 
