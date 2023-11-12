c5f10da61cbf61a8ab29d96ef116d485917f0669
tigris24 <kyrae604@naver.com>
2023-11-13 02:39:36 +0900

    update code and fix some errors
    make methods about numOfSeats
    fix errors about type casting

--- Ticketing/src/main/java/com/ticket/Ticketing/service/SeatService.java
+++ Ticketing/src/main/java/com/ticket/Ticketing/service/SeatService.java
@@ -28,11 +28,18 @@ public class SeatService {
             String documentId = eventId + "-" + String.format("%03d", i);
             if (!documentExists(seatCollection, documentId)) {   // don't make initial documents if already exist
                 JsonObject jsonData = JsonObject.create()
-                        .put("id", String.format("%03d", i))
+                        .put("id", documentId)
                         .put("sold", false);
                 seatCollection.insert(documentId, jsonData);
             }
         }
+    }
+
+    public String numOfSeats(String eventId) {
+        Bucket eventBucket = cluster.bucket(EventConfig.getStaticBucketName());
+        Collection eventCollection = eventBucket.defaultCollection();
+
+        return String.valueOf(eventCollection.get(eventId).contentAsObject().get("seatNum"));
     }
 
     public void deleteSeatByEventDocuments(String eventId, Integer seatNum) {
@@ -97,19 +104,19 @@ public class SeatService {
         return seatReserved;
     }
 
-    public void updateSeat(List<Integer> seatList) {
+    public void updateSeat(List<Integer> seatList, String eventId) {
         Bucket seatBucket = cluster.bucket(SeatConfig.getStaticBucketName());
         Collection seatCollection = seatBucket.defaultCollection();
 
         // update seat info from seat bucket
-        for (Integer seatNum : seatList) {
-            String documentId = String.format("%03d", seatNum);
+        for (int i = 0; i < seatList.size(); i++) {
+            String documentId = eventId + "-" + seatList.get(i);
 
             // check if seat already sold out
             Object seatResult = seatCollection.get(documentId).contentAsObject().get("sold");
             if (seatResult.equals(false)) {
                 JsonObject jsonData = JsonObject.create()
-                        .put("id", String.format("%03d", Integer.valueOf(documentId)))
+                        .put("id", documentId)
                         .put("sold", true);
                 seatCollection.replace(documentId, jsonData);
             }
