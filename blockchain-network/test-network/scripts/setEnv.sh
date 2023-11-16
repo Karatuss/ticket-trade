1da9f1d959c757baf4b8686899d1bda02be9ada0
Karatus <shm1193@gmail.com>
2023-11-16 15:11:11 +0900

    Add: bring folders from fabric-samples
    Brought two folders
    1. asset-transfer-basic/chaincode-go
    2. asset-transfer-basic/test-network

---
+++ blockchain-network/test-network/scripts/setEnv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. scripts/utils.sh
+
+export FABRIC_CFG_PATH=$PWD/../config/
+
+setGlobals() {
+  local USING_ORG=""
+  if [ -z "$OVERRIDE_ORG" ]; then
+    USING_ORG=$1
+  else
+    USING_ORG="${OVERRIDE_ORG}"
+  fi
+  infoln "Using organization ${USING_ORG}"
+  if [ $USING_ORG -eq 1 ]; then
+    export CORE_PEER_TLS_ENABLED=true
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+  elif [ $USING_ORG -eq 2 ]; then
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+  else
+    errorln "ORG Unknown"
+  fi
+}
