09272d735036b8a39e305d5df16959b6c5123dcc
Karatus <shm1193@gmail.com>
2023-11-18 15:12:14 +0900

    Add: application-go JAVA VERSION
    if a developer wants to access the chaincode with Java,
    he/she can use this.

---
+++ Ticketing/src/main/java/com/ticket/Ticketing/applicationGo/StreamGobbler.java
@@ -0,0 +1,26 @@
+package com.ticket.Ticketing.applicationGo;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
+/**
+ * https://www.baeldung.com/run-shell-command-in-java#Output
+ */
+public class StreamGobbler implements Runnable {
+
+    private InputStream inputStream;
+    private Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        new BufferedReader(new InputStreamReader(inputStream)).lines()
+                .forEach(consumer);
+    }
+}
